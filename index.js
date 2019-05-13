106850ccb8279b93b3022a7419da8426a1eb818c
Joel Corey <joelcorey@fastmail.com>
2019-05-13 07:30:51 -0700

    counting sheep function

--- index.js
+++ index.js
@@ -3,77 +3,68 @@ function countDown(n) {
   console.log(n)
   countDown(n - 1)
 }
+//countDown(5)
 
 // 01.) Counting Sheep
-
-function countingSheep() {
-
+function countingSheep(number) {
+  if(number === 0) return
+  console.log(`${number}: Another sheep jump over the fence`)
+  countingSheep(number - 1)
 }
+countingSheep(3)
 
 // 02.) Power Calculator
-
 function powerCalculator() {
 
 }
 
 // 03.) Reverse String
-
 function reverseString() {
 
 }
 
 // 04.) nth Triangular Number
-
 function triangularNumber() {
 
 }
 
 // 05.) String Splitter
-
 function stringSplitter() {
 
 }
 
 // 06.) Fibonacci
-
 function fibonacci() {
 
 }
 
 // 07.) Factorial
-
 function factorial() {
 
 }
 
 // 08.) Find a way out of the maze
-
 function maze() {
 
 }
 
 // 09.) Find ALL the ways out of the maze
-
 function mazeAll() {
 
 }
 
 // 10.) Anagrams
-
 function anagrams() {
 
 }
 
 // 11.) Organization Chart
-
 function organizationChart() {
 
 }
 
 // 12.) Binary Representation
-
 function binaryRepresentation() {
 
 }
 
-countDown(5)
